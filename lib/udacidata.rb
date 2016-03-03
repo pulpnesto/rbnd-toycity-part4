a7ff8f4cd38a995fed29c4310f6ab012ed1b0ec5
pulpnesto <pulp.nesto@gmail.com>
2016-03-02 21:45:13 -0700

    Create self.create method

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,28 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+
+	# Class method to initailize Product as wel as write Product to csv file
+  def self.create(attributes = nil)
+
+  	# Initialize new product
+  	object = self.new(attributes)
+
+  	# Append product data to csv data file.
+  	@data_path = File.dirname(__FILE__) + "/../data/data.csv"
+  	CSV.open(@data_path, "ab") do |csv|
+  		csv << [object.id, object.brand, object.name, object.price]
+  	end
+
+  	# Retrun product
+  	object
+  # If the object's data is already in the database
+  # create the object
+  # return the object
+
+  # If the object's data is not in the database
+  # create the object
+  # save the data in the database
+  # return the object
+	end
 end
