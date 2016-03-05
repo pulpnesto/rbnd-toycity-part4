aba6181322c6fd50f63e36b87051632a59f2ae6b
pulpnesto <pulp.nesto@gmail.com>
2016-03-05 11:12:04 -0700

    Create self.all method and augment self.create method

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -4,20 +4,23 @@ require 'csv'
 
 class Udacidata
 
+DATA_PATH = File.dirname(__FILE__) + "/../data/data.csv"
+
 	# Class method to initailize Product as wel as write Product to csv file
   def self.create(attributes = nil)
 
   	# Initialize new product
   	object = self.new(attributes)
 
-  	# Append product data to csv data file.
-  	@data_path = File.dirname(__FILE__) + "/../data/data.csv"
-  	CSV.open(@data_path, "ab") do |csv|
-  		csv << [object.id, object.brand, object.name, object.price]
+  	if attributes[:id] == nil
+  		# Append product data to csv data file.
+  		CSV.open(DATA_PATH, "ab") do |csv|
+  			csv << [object.id, object.brand, object.name, object.price]
+  		end
   	end
-
   	# Retrun product
   	object
+
   # If the object's data is already in the database
   # create the object
   # return the object
@@ -27,4 +30,15 @@ class Udacidata
   # save the data in the database
   # return the object
 	end
+
+	def self.all
+		object_array = []
+	  tmp_array = CSV.read(DATA_PATH, {encoding: "UTF-8",headers: true, header_converters: :symbol, converters: :all})
+    tmp_array.each do |row|
+    	row[:name] = row[:product]
+    	object_array << Product.create(row)
+    end
+    object_array
+	end
+
 end
