c87027b9b5f15d943f020a23729e0b9e56381e82
pulpnesto <pulp.nesto@gmail.com>
2016-03-05 11:30:00 -0700

    Create self.first method and refactor CSV.read

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -12,33 +12,40 @@ DATA_PATH = File.dirname(__FILE__) + "/../data/data.csv"
   	# Initialize new product
   	object = self.new(attributes)
 
+  	# Check to see if the id field is empty.
   	if attributes[:id] == nil
   		# Append product data to csv data file.
   		CSV.open(DATA_PATH, "ab") do |csv|
   			csv << [object.id, object.brand, object.name, object.price]
   		end
   	end
-  	# Retrun product
+  	# Retrun object
   	object
-
-  # If the object's data is already in the database
-  # create the object
-  # return the object
-
-  # If the object's data is not in the database
-  # create the object
-  # save the data in the database
-  # return the object
 	end
 
+	# Class method to return all objects in csv file
 	def self.all
 		object_array = []
-	  tmp_array = CSV.read(DATA_PATH, {encoding: "UTF-8",headers: true, header_converters: :symbol, converters: :all})
-    tmp_array.each do |row|
+    csv_table = get_table
+    csv_table.each do |row|
     	row[:name] = row[:product]
-    	object_array << Product.create(row)
+    	object_array << self.create(row)
     end
     object_array
 	end
 
+	def self.first
+    csv_table = get_table
+		row = csv_table.first
+		row[:name] = row[:product]
+		self.create(row)
+	end
+
+  private
+	  def self.get_table
+	    CSV.read(DATA_PATH, {encoding: "UTF-8",headers: true, header_converters: :symbol, converters: :all})
+	  end
 end
+
+
+#puts csv.find {|row| row['NAME'] == 'Tom'}
