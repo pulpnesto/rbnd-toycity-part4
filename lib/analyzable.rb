b5c6afe5b6485f8a3f43122ddc20358b14c404f1
pulpnesto <pulp.nesto@gmail.com>
2016-03-12 15:41:47 -0700

    Create count_by_brand and _name methods

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,8 +1,21 @@
 module Analyzable
-  # Your code goes here!
   def average_price(object_array)
   	length = object_array.length
   	(object_array.inject(0){|sum,el| sum + el.price} / length).round(2)
-
   end
+
+  # Method to count occurences of a brand in an array
+  # Used code found at jerodsanto.net
+  def count_by_brand(object_array)
+  	object_array.each_with_object(Hash.new(0)) {|obj,counts| counts[obj.brand] += 1}
+  end
+
+  def count_by_name(object_array)
+  	object_array.each_with_object(Hash.new(0)) {|obj,counts| counts[obj.name] += 1}
+  end
+
+  def print_report(object_array)
+  	arg = "Hello"
+  end
+
 end
